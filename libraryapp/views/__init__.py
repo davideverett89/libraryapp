b0e18ec6a104d9ce61530a8e7c5026789d504b3b
David Everett <davideverett1989@gmail.com>
2020-08-06 14:27:23 -0500

    Finished chapter 6.

--- libraryapp/views/__init__.py
+++ libraryapp/views/__init__.py
@@ -6,4 +6,5 @@ from .books.form import book_form
 from .libraries.form import library_form
 from .books.details import book_details
 from .libraries.details import library_details
+from .librarians.details import librarian_details
 from .home import home
